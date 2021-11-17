84fae1b2df6affc731b6ad3c38511a37e505a8d3
Stephan Klinger <staeff@arcor.de>
2021-11-17 17:29:36 +0100

    Improve readability for parameterized test

--- test_wallet.py
+++ test_wallet.py
@@ -36,10 +36,12 @@ def my_wallet():
     '''Returns a Wallet instance with zero balance'''
     return Wallet()
 
-@pytest.mark.parametrize('earned,spent,expected', [
+amounts = [
     (30, 10, 20),
     (20, 2, 18)
-])
+]
+
+@pytest.mark.parametrize('earned,spent,expected', amounts)
 def test_transactions(my_wallet, earned, spent, expected):
     my_wallet.add_cash(earned)
     my_wallet.spend_cash(spent)
