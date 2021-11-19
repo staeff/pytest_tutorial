81359cb2fd0f059567b48c41e7b222f80161dc48
Stephan Klinger <staeff@arcor.de>
2021-11-19 18:04:33 +0100

    Add another function and tests to cube.py

--- cube.py
+++ cube.py
@@ -9,3 +9,18 @@ def cube(n):
         int: The cubed result
     '''
     return n ** 3
+
+def cube_root(n):
+    '''
+    Returns the cube root of the input number
+
+    Args:
+        n (int): The number to cube root
+
+    Returns:
+        int: The cube root result
+    '''
+    if n >= 0:
+        return n ** (1/3)
+    else:
+        raise ValueError('A number larger than 0 was expected')
--- test_cube.py
+++ test_cube.py
@@ -19,3 +19,18 @@ def test_no_arguments():
 def test_exception_str():
     with pytest.raises(TypeError):
         cube.cube('x')
+
+cube_roots = (
+    (0, 0),
+    (1, 1),
+    (8, 2),
+    (27, 3),
+)
+
+@pytest.mark.parametrize('n, expected', cube_roots)
+def test_cube_root(n, expected):
+    assert cube.cube_root(n) == expected
+
+def test_cube_root_below_zero():
+    with pytest.raises(ValueError):
+        cube.cube_root(-1)
